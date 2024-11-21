351b77ace49c976225f27e8228ee05ca2eb15f5d
Denuwan Nammuni <denuwannammuni@gmail.com>
2024-11-21 23:33:40 +0530

    test.js update

--- dark_mode/src/components/test/test.js
+++ dark_mode/src/components/test/test.js
@@ -5,6 +5,10 @@ export default function test() {
     return (
         <>
             <div className='wrapper'>
+                <label className="switch">
+                    <input type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
 
             </div>
         </>
