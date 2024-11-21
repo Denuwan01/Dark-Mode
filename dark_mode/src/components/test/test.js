41f28c9b28f0fe8c4c81350128617de1f55fcf9b
Denuwan Nammuni <denuwannammuni@gmail.com>
2024-11-21 23:20:34 +0530

    modify

---
+++ dark_mode/src/components/test/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './test.css'
+
+export default function test() {
+    return (
+        <>
+            <div className='wrapper'>
+
+            </div>
+        </>
+    )
+}
