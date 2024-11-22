724cc40799dabaeebdcdaecdf426a9928a2db0e0
Denuwan Nammuni <denuwannammuni@gmail.com>
2024-11-22 23:50:04 +0530

    modify

--- dark_mode/src/App.js
+++ dark_mode/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/test/test';
 
 function App() {
   return (
--- dark_mode/src/components/test/test.js
+++ dark_mode/src/components/test/test.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './test.css'
 
-export default function test() {
+export default function Test() {
     return (
         <>
             <div className='wrapper'>
                 <label className="switch">
-                    <input type="checkbox" />
-                    <span className="slider round"></span>
+                <input type="checkbox" />
+                <span className="slider round"></span>
                 </label>
 
             </div>
