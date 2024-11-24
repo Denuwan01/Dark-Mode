63a03b2fffebbc26922d79b1e8e4a480f63346db
Denuwan Nammuni <denuwannammuni@gmail.com>
2024-11-24 21:50:47 +0530

    modify

--- dark_mode/src/components/test/test.js
+++ dark_mode/src/components/test/test.js
@@ -2,12 +2,21 @@ import React from 'react'
 import './test.css'
 
 export default function Test() {
+
+    const switchTheme = (e) => {
+        if (e.target.checked) {
+            document.querySelector('body').setAttribute('data-theme', 'dark')
+        }
+        else {
+            document.querySelector('body').setAttribute('data-theme', 'light')
+        }
+    }
     return (
         <>
             <div className='wrapper'>
                 <label className="switch">
-                <input type="checkbox" />
-                <span className="slider round"></span>
+                    <input type="checkbox" />
+                    <span className="slider round"></span>
                 </label>
 
             </div>
